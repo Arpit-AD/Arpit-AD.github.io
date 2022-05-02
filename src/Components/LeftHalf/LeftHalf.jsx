938278d8cf319c694f1c82324d24341c9eb53f07
Arpit <arpit2001.ad@gmail.com>
2022-05-02 21:52:36 +0530

    portfolio updated

--- src/Components/LeftHalf/LeftHalf.jsx
+++ src/Components/LeftHalf/LeftHalf.jsx
@@ -15,7 +15,7 @@ const LeftHalf  = () =>{
       <div className=' md:block hidden'>
         <Links />
         </div>
-          <div className='font-sans md:hidden block xs:text-2xl text-xl leading-loose text-2xl font-semibold text-gray-500 font-bold m-10 w-fit tracking-wider xl:p-10 p-6 xs:p-6 p-0 md:m-10 m-6 xs:m-6 m-2 md:mt-0 mt-16'>
+          <div className='font-sans md:hidden block xs:text-2xl text-xl leading-loose text-2xl font-semibold text-gray-500 font-bold m-10 w-fit tracking-wider xl:p-10 p-6 xs:p-6 p-0 md:m-10 m-6 xs:m-6 m-2 md:mt-0 mt-16  transition duration-200 ease-out transition-all animate-slide-in'>
              <span className='xl:text-3xl text-2xl xs:text-2xl text-base font-semibold text-gray-400 font-bold '>Hello, This is </span><span className='xl:text-6xl text-5xl xs:text-5xl text-3xl font-bold text-white'>Arpit </span><br />
               <div className='xl:text-2xl text-xl xs:text-xl text-base text-gray-400 xl:p-4 p-2 font-bold leading-tight'>A <span className='text-gray-100'>Software Developer </span> & <span className='text-gray-100'> Designer</span><br/>
                   Passionate to make life easy and amazing!!!
--- src/Components/RightHalf/RightHalf.jsx
+++ src/Components/RightHalf/RightHalf.jsx
@@ -6,15 +6,15 @@ const RightHalf  = () =>{
 
   return (
       <div className='min-h-screen md:w-1/2 w-1/6  light-gradient-bg'>
-          <div className='font-sans leading-loose text-2xl md:block hidden font-semibold text-gray-500 font-bold m-10 w-fit tracking-wider xl:p-10 p-6'>
-             <span className='xl:text-3xl text-2xl font-semibold text-gray-400 font-bold '>Hello, This is </span><span className='xl:text-6xl text-5xl font-bold text-lightslate'>Arpit </span><br />
+          <div className='font-sans leading-loose text-2xl md:block hidden font-semibold text-gray-500 font-bold m-10 w-fit tracking-wider xl:p-10 p-6  transition duration-200 ease-out transition-all animate-slide-in'>
+             <span className='xl:text-3xl text-2xl font-semibold text-gray-400 font-bold '>Hello, This is </span><span className='xl:text-6xl text-5xl font-bold text-lightslate '>Arpit </span><br />
               <div className='xl:text-2xl text-xl text-gray-400 xl:p-4 p-2 font-bold leading-tight'>A <span className='text-gray-600'>Software Developer </span> & <span className='text-gray-600'> Designer</span><br/>
                   Passionate to make life easy and amazing!!!
                   </div>
           </div>
           <div className='text-gray-500 w-fit text-lg tracking-widest absolute  bottom-1/2 md:-right-8 -right-12 rotate-90 '> (+91) - 77019 61032</div>
           <a className='w-fit xl:text-2xl text-xl md:text-xl text-md md:px-7 px-0 flex md:py-3 py-3 text-gray-500 absolute font-semibold  bottom-10 right-2 tracking-widest'>
-            <span className='md:inline hidden '>RESUME</span>    <a href='https://drive.google.com/file/d/1bp2r4Ct6jE7MgOT_dw5jB5anklBLiggd/view?usp=sharing' target="_blank" > <BsArrowUpRightCircleFill className='text-slate-600 hover:text-darkslate h-8 mx-1 w-10 '/> </a>
+            <span className='md:inline hidden '>RESUME</span>    <a href='https://drive.google.com/file/d/1VHexmS8kfcCB5qshpZ22LKFKsdxA2r9k/view?usp=sharing' target="_blank" > <BsArrowUpRightCircleFill className='text-slate-600 hover:text-darkslate h-8 mx-1 w-10 '/> </a>
           </a>
     </div>
   )
