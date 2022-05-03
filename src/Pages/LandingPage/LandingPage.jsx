c5bb834619889706a99e5727845bc7175c80ab24
Arpit <arpit2001.ad@gmail.com>
2022-05-03 12:56:16 +0530

    Photo updated

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -8,9 +8,9 @@ function LandingPage() {
   return (
       <div className="h-screen flex items-center">
       <LeftHalf />
-      <img src={mypic} className='3lg/2:hidden block fixed md:inset-x-1/4 -right-5 bottom-0  w-9/12 imageScreen:w-[320px]  3lg/2:w-[420px] '></img>
-      <img src={mypic2} className='3lg/2:block hidden fixed md:inset-x-1/4 -right-5 bottom-0  w-9/12 imageScreen:w-[320px]  3lg/2:w-[500px] '></img>
-      <RightHalf />
+      <img src={mypic} className='3lg/2:hidden block fixed md:inset-x-1/4 -right-12 bottom-0 h-2/3 '></img>
+      <img src={mypic2} className='3lg/2:block hidden fixed md:inset-x-1/4  bottom-0 h-5/6'></img>
+      <RightHalf/>
       {/* {screen === "NotLanding" && <LeftHalfScreen/>}
       {screen === "NotLanding" && <RightHalfScreen/>} */}
       </div>
