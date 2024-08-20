0811cb1c5e6dc11475345f1a8f43a37cc1c73b8d
Arpit Das <arpit2001.ad@gmail.com>
2024-08-20 17:26:33 +0530

    new resume added

--- src/Components/Links/Links.jsx
+++ src/Components/Links/Links.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import { ImLinkedin ,ImGithub, ImTwitter, ImInstagram} from "react-icons/im"
-import { SiLeetcode, SiCodechef, SiCodeforces} from "react-icons/si"
-import {BsArrowUpRightCircleFill} from "react-icons/bs"
+import React from "react";
+import { ImLinkedin, ImGithub, ImTwitter, ImInstagram } from "react-icons/im";
+import { SiLeetcode, SiCodechef, SiCodeforces } from "react-icons/si";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
 
+const Links = () => {
+	return (
+		<div className="text-gray-400 w-1/4 my-20 md:mx-12 mx-6">
+			<ul>
+				<li>
+					<a href="https://github.com/Arpit-AD/" target="_blank">
+						<ImGithub className="hover:text-white h-8 w-5" />
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://www.linkedin.com/in/arpit-das-9210a01a5/"
+						target="_blank"
+					>
+						<ImLinkedin className="hover:text-white h-8 w-5" />
+					</a>
+				</li>
+				<li>
+					<a href="https://leetcode.com/adarpit/" target="_blank">
+						<SiLeetcode className="hover:text-white h-8 w-5" />
+					</a>
+				</li>
+				{/* <li><a href="https://www.codechef.com/users/adarpit" target="_blank"><SiCodechef className='hover:text-white h-8 w-5'/></a></li> */}
+				<li>
+					<a href="https://codeforces.com/profile/Arpitad" target="_blank">
+						<SiCodeforces className="hover:text-white h-8 w-5" />
+					</a>
+				</li>
+				<li>
+					{" "}
+					<a href="https://bit.ly/ad-aug24" target="_blank">
+						{" "}
+						<BsArrowUpRightCircleFill className="hover:text-white h-8 w-5" />{" "}
+					</a>
+				</li>
+				{/* <li><a href="https://www.instagram.com/arpit_ad_/" target="_blank"><ImInstagram className='hover:text-white h-8 w-5'/></a></li> */}
+			</ul>
+		</div>
+	);
+};
 
-const Links  = () =>{
-      
-
-  return (
-      <div className='text-gray-400 w-1/4 my-20 md:mx-12 mx-6'>
-          <ul>
-          <li><a href="https://github.com/Arpit-AD/" target="_blank" ><ImGithub  className='hover:text-white h-8 w-5'/></a></li>
-          <li><a href="https://www.linkedin.com/in/arpit-das-9210a01a5/" target="_blank"><ImLinkedin className='hover:text-white h-8 w-5'/></a></li>
-          <li><a href="https://leetcode.com/adarpit/" target="_blank"><SiLeetcode className='hover:text-white h-8 w-5'/></a></li>
-          <li><a href="https://www.codechef.com/users/adarpit" target="_blank"><SiCodechef className='hover:text-white h-8 w-5'/></a></li>
-          <li><a href="https://codeforces.com/profile/Arpitad" target="_blank"><SiCodeforces className='hover:text-white h-8 w-5'/></a></li>
-          <li> <a href='https://bit.ly/ad-aug' target="_blank" > <BsArrowUpRightCircleFill className='hover:text-white h-8 w-5'/> </a></li>
-          {/* <li><a href="https://www.instagram.com/arpit_ad_/" target="_blank"><ImInstagram className='hover:text-white h-8 w-5'/></a></li> */}
-          </ul>
-    </div>
-  )
-}
-
-export default Links
+export default Links;
