e4d682937b00e9f3088e31d8fb0f0fb806e3426f
Arpit Das <arpit2001.ad@gmail.com>
2024-08-20 17:22:58 +0530

    updated with hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import { LandingPage } from './Pages'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import ProfileScreen from './Components/RightHalfScreen/ProfileScreen/ProfileScreen'
-import NotLandingPage from './Pages/LandingPage/NotLandingPage'
-import ProjectScreen from './Components/RightHalfScreen/ProjectScreen/ProjectScreen'
-import ContactScreen from './Components/RightHalfScreen/ContactScreen/ContactScreen'
-import ExperienceScreen from './Components/RightHalfScreen/ExperienceScreen/ExperienceScreen'
+import { useState } from "react";
+import "./App.css";
+import { LandingPage } from "./Pages";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import ProfileScreen from "./Components/RightHalfScreen/ProfileScreen/ProfileScreen";
+import NotLandingPage from "./Pages/LandingPage/NotLandingPage";
+import ProjectScreen from "./Components/RightHalfScreen/ProjectScreen/ProjectScreen";
+import ContactScreen from "./Components/RightHalfScreen/ContactScreen/ContactScreen";
+import ExperienceScreen from "./Components/RightHalfScreen/ExperienceScreen/ExperienceScreen";
 
 function App() {
-
-  return (
-    <div className="">
-      <BrowserRouter >
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path='/profile' element={<NotLandingPage Right={ProfileScreen} />} />
-          <Route path='/projects' element={<NotLandingPage Right={ProjectScreen} />} />
-          <Route path='/experience' element={<NotLandingPage Right={ExperienceScreen} />} />
-          <Route path='/contact' element={<NotLandingPage Right={ContactScreen} />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+	return (
+		<div className="">
+			<Router>
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+					<Route
+						path="/profile"
+						element={<NotLandingPage Right={ProfileScreen} />}
+					/>
+					<Route
+						path="/projects"
+						element={<NotLandingPage Right={ProjectScreen} />}
+					/>
+					<Route
+						path="/experience"
+						element={<NotLandingPage Right={ExperienceScreen} />}
+					/>
+					<Route
+						path="/contact"
+						element={<NotLandingPage Right={ContactScreen} />}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { RightHalf, LeftHalf} from '../../Components'
-import mypic from "../../images/mypic.png"
-import mypic2 from "../../images/mypic2.png"
-import icon from "../../../logo.png"
+import React from "react";
+import { RightHalf, LeftHalf } from "../../Components";
+import icon from "../../../logo.png";
 
 function LandingPage() {
-  // const [screen, setScreen] = useState("Landing");
-  return (
-      <div className="h-screen flex items-center">
-      <LeftHalf />
+	// const [screen, setScreen] = useState("Landing");
+	return (
+		<div className="h-screen flex items-center">
+			<LeftHalf />
 
-      {/* <img src={mypic} className='md:hidden block fixed bottom-0 -right-12  h-2/3 '></img> */}
-      {/* <img src={mypic2} className='md:block hidden fixed bottom-0 -right-12  h-2/3'></img> */}
-      <img src={icon} className='md:block hidden fixed z-10 md:inset-x-20 opacity-10  bottom-0 h-3/4'></img>
+			{/* <img src={mypic} className='md:hidden block fixed bottom-0 -right-12  h-2/3 '></img> */}
+			{/* <img src={mypic2} className='md:block hidden fixed bottom-0 -right-12  h-2/3'></img> */}
+			<img
+				src={icon}
+				className="md:block hidden fixed z-10 md:inset-x-20 opacity-10  bottom-0 h-3/4"
+			></img>
 
-      <RightHalf/>
-      {/* {screen === "NotLanding" && <LeftHalfScreen/>}
+			<RightHalf />
+			{/* {screen === "NotLanding" && <LeftHalfScreen/>}
       {screen === "NotLanding" && <RightHalfScreen/>} */}
-      </div>
-  )
+		</div>
+	);
 }
 
-export default LandingPage
+export default LandingPage;
